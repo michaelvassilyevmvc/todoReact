ab7b88d08bbc1717fd7adfc676510334d1dd0e3f
vassilyev <vassilyev@toowto.kz>
2022-06-01 17:57:14 +0600

    Lesson 2 timeslot 1-04

---
+++ src/components/AddList/index.jsx
@@ -0,0 +1,46 @@
+import React, { Fragment } from "react";
+import List from "../List";
+
+import "./AddButtonList.scss";
+
+const AddListButton = () => {
+  return (
+    <Fragment>
+      <List
+        items={[
+          {
+            className: "list__add-button",
+            icon: (
+              <svg
+                width="12"
+                height="12"
+                viewBox="0 0 16 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M8 1V15"
+                  stroke="black"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M1 8H15"
+                  stroke="black"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            ),
+            name: "Добавить список",
+          },
+        ]}
+      ></List>
+      <div className="add-list-popup"></div>
+    </Fragment>
+  );
+};
+
+export default AddListButton;
